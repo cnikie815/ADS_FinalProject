19d60bbb68408b3d81da3b433d7731e346956d68
Cam <Cam@DESKTOP-POEKOJ7.home>
2017-05-08 21:26:04 -0400

    efewf

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,65 @@
-import java.util.*;
-public class Card
-{
-    public static void main (String[] args) {
-        PlayingCard c1 = new PlayingCard(14, 3);
-        for (int i = 0; i < 21; i++) {
-            c1.genRand();
-            System.out.println(c1);
-        }
-    }
+
+public class Card implements Comparable{
+	private Suit s;
+	private boolean up;
+	private int n;
+	public Card (Suit suit, int num){
+		s = suit;
+		num = num;
+		up = false;
+	}
+	public boolean equals(Object c){
+		if(this==c){
+			return true;
+		} else if(! (c instanceof Card)){
+			return false;
+		} else {
+			Card c2 = (Card)c;
+			return n == c2.n;
+		}
+	}
+	public int compare(Object c){
+		if (!(c instanceof Card)){
+			throw new IllegalArgumentException("not a card");
+		}
+		Card c2 = (Card)c;
+		return n - c2.n;
+	}
+	public int getSuit(){
+		return s;
+	}
+	public int getN(){
+		return n;
+	}
+	public boolean isHeartOrDiamond(){
+		return s == Suit.diamond || Suit.heart;
+	}
+	public boolean isUp(){
+		return up;
+	}
+	public void turn(){
+		up = !up;
+	}
+	public String toString(){
+		return numToString() + " of " + s;
+	}
+	private String numToString(){
+		if(n == 11){
+			return "Jack";
+		} else if( n == 12){
+			return "Queen";
+		} else if(n == 13){
+			return "King";
+		} else if (n == 1){
+			return "Ace";
+		} else {
+			return "" + n;
+		}
+		
+	}
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
---
+++ src/Deck.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+	public static final int totalSize = 0;
+	private ArrayList<Card> cards;
+	public Deck(){
+		reset();
+	}
+	private void reset(){
+		cards = new ArrayList<Card>();
+		addSuit(Suit.spade);
+		addSuit(Suit.heart);
+		addSuit(Suit.diamond);
+		addSuit(Suit.club);
+	}
+	private void addSuit(Suit s){
+		for (int a = 0; a < 14; a++){
+			cards.add(new Card(s, a));
+		}
+	}
+	public boolean empty(){
+		return cards.isEmpty();
+	}
+	public void clear(){
+		cards.clear();
+	}
+	public void add(Card c){
+		cards.add(c);
+	}
+	public Card deal(){
+		if (empty()){
+			return null;
+		} else {
+			return cards.remove(cards.size() -1);
+		}
+	}
+	public Card[] deal(int n){
+		if (n > cards.size()){
+			return null;
+		} else {
+			Card[] player = new Card[n];
+			for (int a = 0; a < player.length; a++){
+				player[a] = deal();
+				return player;
+		}
+		}
+		return null;
+	}
+	public Card getTop(){
+		return cards.remove(0);
+	}
+	public int size(){
+		return cards.size();
+	}
+	public void shuffle(){
+		if(cards.size() < totalSize){
+			return;
+		}
+		Random x = new Random();
+		Card[] array = new Card[totalSize];
+		while (cards.size() > 0){
+			Card c = cards.remove(cards.size() - 1);
+			int a = x.nextInt(totalSize);
+			while ( array[a] != null){
+				a = x.nextInt(totalSize);
+				array[a] = c;
+			}
+		}
+		for (Card c : array){
+			cards.add(c);
+		}
+	}
+	public String toString(){
+		String z = "";
+		for (Card c: cards){
+			z+= c + "\n";
+		}
+		return z;
+	}
+}
