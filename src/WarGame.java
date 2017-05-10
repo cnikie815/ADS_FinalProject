c5300e223af6ac61c0aef011a8bfe47b7160f32a
Cam <Cam@DESKTOP-POEKOJ7.home>
2017-05-09 20:57:42 -0400

    fewf

--- src/WarGame.java
+++ src/WarGame.java
@@ -9,25 +9,27 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class WarGame {
+	private static final CardName JackSpade = null;
+
 	public static void main(String args[]){
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
 		JPanel p = new JPanel();
 		BoxLayout b = new BoxLayout(p, BoxLayout.PAGE_AXIS);
 		JButton go = new JButton("press");
-		go.setAlignmentX(p.CENTER_ALIGNMENT);
+		go.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel playerDiscard = new JLabel("playerDiscard");
-		playerDiscard.setAlignmentX(p.CENTER_ALIGNMENT);
+		playerDiscard.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel playerDeck = new JLabel("playerDeck");
-		playerDeck.setAlignmentX(p.CENTER_ALIGNMENT);
+		playerDeck.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel playerRun = new JLabel("playerRun");
-		playerRun.setAlignmentX(p.CENTER_ALIGNMENT);
+		playerRun.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel compRun = new JLabel("compRun");
-		compRun.setAlignmentX(p.CENTER_ALIGNMENT);
+		compRun.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel compDiscard = new JLabel("compDiscard");
-		compDiscard.setAlignmentX(p.CENTER_ALIGNMENT);
+		compDiscard.setAlignmentX(Component.CENTER_ALIGNMENT);
 		JLabel compDeck = new JLabel("compDeck");
-		compDeck.setAlignmentX(p.CENTER_ALIGNMENT);
+		compDeck.setAlignmentX(Component.CENTER_ALIGNMENT);
 		p.setLayout(b);
 		p.add(go);
 		p.add(playerDiscard);
@@ -39,14 +41,13 @@ public class WarGame {
 		f.add(p);
 		f.pack();
 		f.setVisible(true);
-		/*
 		int half;
 		Deck d = new Deck();
 		Deck d1 = new Deck();
 		Deck d2 = new Deck();
 		for (int a = 2; a < 15; a++){
 			if (a == 11){
-				d.add(new Card("Jack of Spades"));
+				d.add(new Card(JackSpade));
 				d.add(new Card("Jack of Diamonds"));
 				d.add(new Card("Jack of Clubs"));
 				d.add(new Card("Jack of Hearts"));
@@ -73,6 +74,6 @@ public class WarGame {
 			}
 		d.shuffle();
 		half = d.size()/2;
-		*/
 	}
+}
 }
